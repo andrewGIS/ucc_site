91d2fc3d5eaf2eeda1a3e44168bf5717c37b9fb5
Mister A <andrew.tarasov1993@gmail.com>
2019-05-09 00:55:51 +0500

    work_verison_of_change_indicator

--- src/index.ts
+++ src/index.ts
@@ -16,19 +16,27 @@ import * as _ from "lodash";
 //let container = Creat
 //let map = new L.Map(document.getElementById("map"));
 
+
+interface month {
+    key:string,
+    alias:string
+}
+
 var start_index: number = 0;
-var copy_data = loaded_data;
+//var copy_data = loaded_data;
 var copy_data_v2 = loaded_data_v2;
-var first_layer = copy_data["data"]["rasters"][start_index].raster.raster_name
-var start_indicator_name = "Day_with_snow"
+var first_layer = "ucc:Day_with_snow_1951_1980_with_h_focal.tif"
+var start_indicator_name = "Mean_pressure"
+var start_period = "1981_2010"
 var start_group_name = "Climate_terms_3_hours_data_focal";
-
-
-var active_period_index = -1;
+var first_month = "year";
+var first_postfix = "_focal.tif"
+var first_style = "ucc:pressure_style"
+var init_month: month[] =[{key:"jan",alias:"Январь"}]
 
 
 var layer_wms: L.TileLayer.WMS
-console.log(first_layer);
+//console.log(first_layer);
 layer_wms = new L.TileLayer.WMS('http://localhost:8080/geoserver/ucc/ows?',
     {
         layers: first_layer,
@@ -41,12 +49,12 @@ layer_wms = new L.TileLayer.WMS('http://localhost:8080/geoserver/ucc/ows?',
 
 document.addEventListener("DOMContentLoaded", function (event) {
     //load_map();
-    init_apps();
+    init_app();
 
 });
 
 
-function init_apps() {
+function init_app() {
 
     // let slider_app = new Vue({
     //     el: '#slider-app',
@@ -77,108 +85,123 @@ function init_apps() {
 
 
 
-    let main_app = new Vue({
+    var main_app = new Vue({
         el: '#main_app',
         data: {
-            temp_active: false,
-            loaded_rasters: copy_data,
+            temp_active: true,
+            loaded_rasters: copy_data_v2,
             selected_indicator: start_indicator_name,
             selected_group: start_group_name,
-            current_year: "Выберите год",
+            selected_period: start_period,
+            selected_month: first_month,
+            selected_postfix: first_postfix,
+            selected_style: first_style,
+            aviable_months: init_month,
+            aviable_periods: ["1981_2010"],
             map: load_map(),
-            periods: ""
 
         },
         methods: {
             show_temp: function () {
                 this.temp_active ? this.temp_active = false : this.temp_active = true;
             },
-            change_period: function (event: any) {
-                this.current_year = event.explicitOriginalTarget._value
-            },
-            change_indicator: function (event: any) {
-                // console.log(event.explicitOriginalTarget._value);
+            change_indicator: function (this: any, event: any) {
                 this.selected_indicator = event.explicitOriginalTarget._value
+                //console.log("change_indicator => " + this.selected_indicator);
+                this.update_postfix();
+                this.update_style();
+                this.update_month();
+                this.update_period();
+                this.update_map();
+
             },
-            decrease_year: function () {
-                start_index = start_index - 1
-                //this.current_year -= 1;
-                layer_wms.setParams({ layers: copy_data["data"]["rasters"][start_index].raster.raster_name }, false);
-                // console.log(copy_data["data"]["rasters"][start_index+1].raster.raster_name);
-                this.current_year = copy_data["data"]["rasters"][start_index].raster.period
-                active_period_index -= 1;
+            update_map: function (this: any) {
+                //console.log(`ucc:${this.selected_indicator}_${this.current_year}_${this.selected_month}_${this.selected_postfix}`)
+                //console.log(this.selected_month)
+                //console.log(this.selected_month)
+                if (this.selected_month == "None") {
+                    console.log(`ucc:${this.selected_indicator}_${this.selected_period}_${this.selected_postfix}`)
+                    layer_wms.setParams(
+                        { layers: `ucc:${this.selected_indicator}_${this.selected_period}_${this.selected_postfix}`, styles: this.selected_style }, false)
+
+                } else {
+                    console.log(`ucc:${this.selected_indicator}_${this.selected_period}_${this.selected_month}_${this.selected_postfix}`)
+                    layer_wms.setParams(
+                        { layers: `ucc:${this.selected_indicator}_${this.selected_period}_${this.selected_month}_${this.selected_postfix}`, styles: this.selected_style }, false)
+                }
+
             },
-            increase_year: function () {
-                start_index = start_index + 1
-                //this.current_year += 1;
-                layer_wms.setParams({ layers: copy_data["data"]["rasters"][start_index].raster.raster_name }, false);
-                // console.log(copy_data["data"]["rasters"][start_index].raster.raster_name);
-                this.current_year = copy_data["data"]["rasters"][start_index].raster.period;
-                active_period_index += 1
+            update_postfix() {
+                this.selected_postfix = _.filter(this.loaded_rasters.groups[0].indicators, (obj) => {
+                    return obj.name === this.selected_indicator
+                })[0].postfix_in_file
+
+                //console.log("change_postfix => " + this.selected_postfix);
             },
-            update_map: function () {
-                console.log(`ucc:${this.selected_indicator}_${this.current_year}_with_h_focal.tif`)
-                layer_wms.setParams({ layers: `ucc:${this.selected_indicator}_${this.current_year}_with_h_focal.tif` }, false)
+            update_style: function () {
+                this.selected_style = _.filter(this.loaded_rasters.groups[0].indicators, (obj) => {
+                    return obj.name === this.selected_indicator
+                })[0].style
+                //console.log("change_style => " + this.selected_style);
+            },
+            update_period: function () {
+
+                let aviable_periods = _.filter(this.loaded_rasters.groups[0].indicators, (obj) => {
+                    return obj.name === this.selected_indicator
+                })[0]
+
+                this.selected_period = aviable_periods.periods[0]
+                this.aviable_periods = aviable_periods.periods
+                return aviable_periods.periods
+            },
+            update_month: function () {
+
+                let aviable_months = _.filter(this.loaded_rasters.groups[0].indicators, (obj) => {
+                    return obj.name === this.selected_indicator
+                })[0]
+
+                this.selected_month = aviable_months.months[0]
+                this.aviable_months = _.map(aviable_months.months, function (month) {
+                    switch (month) {
+                        case "year":
+                            return { key:"year",alias: "Год" }
+                        case "jan":
+                            return { key:"jul",alias: "Январь" }
+                        case "jul":
+                            return { key:"jul",alias: "Июль" }
+                        case "oct":
+                            return { key:"oct",alias: "Октябрь" }
+                        case "apr":
+                            return { key:"jul",alias: "Апрель" }
+                        default:
+                            return { key:"None",alias: "Нет данных" }
+                    }
+                })
+                return aviable_months.months
             }
         },
-        computed:
-        {
-            update_map: function () {
-                console.log(`ucc:${this.selected_indicator}_${this.current_year}_with_h_focal.tif`)
-                layer_wms.setParams({ layers: `ucc:${this.selected_indicator}_${this.current_year}_with_h_focal.tif` }, false)
-            },
-            create_aviable_indicator_list: function () {
+        computed: {
 
-                // return all object where group equals to selected now and create uniq list of indicator_alias in object
-
-                let sorted_objects = _.filter(this.loaded_rasters.data.rasters, (obj) => {
-                    return obj.raster.group_name === this.selected_group
+            aviable_indicator_list: function () {
+                return _.filter(this.loaded_rasters.groups[0].indicators, (obj) => {
+                    return obj.name
                 })
-
-                return _.uniqBy(sorted_objects, function (raster) {
-                    return raster.raster.indicator_alias;
-                })
-
-            },
-            create_aviable_year_list: function () {
-
-                // return all object for selected indicator group
-
-                let sorted_objects = _.filter(this.loaded_rasters.data.rasters, (obj) => {
-                    return obj.raster.indicator_name === this.selected_indicator
-                })
-
-                return _.uniqBy(sorted_objects, function (raster) {
-                    return raster.raster.period;
-                })
-
-            },
-            create_aviable_month_list: function () {
-
-                // return all object for selected indicator group
-
-                let sorted_objects = _.filter(this.loaded_rasters.data.rasters, (obj) => {
-                    return obj.raster.indicator_name === this.selected_indicator
-                })
-
-                return _.uniqBy(sorted_objects, function (raster) {
-                    return raster.raster.month;
-                })
-
             }
-
         }
     })
 }
 
 function load_map(): L.Map {
 
+    //console.log("call build map")
+
     let map = new L.Map("map", {
         minZoom: 4,
-        maxZoom: 5
+        maxZoom: 6
     });
     map.setView(new L.LatLng(59, 59), 4);
-    let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: "attribution test" })
+    let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        { attribution: "attribution test" })
 
     //layer.addTo(map);
     layer_wms.addTo(map);
--- src/indicators_meta_v2.ts
+++ src/indicators_meta_v2.ts
@@ -7,17 +7,27 @@ export const loaded_data = {
                 {
                     name: "Mean_pressure",
                     alias:"Среднее давление",
-                    periods:['1981_2010', '1971_2000'],
+                    periods:['1971_2000','1981_2010'],
                     months:['jul', 'year', 'oct', 'apr', 'jan'],
-                    style:"None"
+                    style:"raster",
+                    postfix_in_file:"focal.tif"
 
                 },
                 {
                     name:"Mean_wind",
-                    alias:"Средння скорость ветра",
-                    periods:['1981_2010', '1971_2000'],
-                    months:['jul', 'year', 'oct', 'apr', 'jan'],
-                    style:"None"
+                    alias:"Средняя скорость ветра",
+                    periods:['1971_2000','1981_2010'],
+                    months:['year','jan','apr', 'jul', 'oct'],
+                    style:"raster",
+                    postfix_in_file:"with_h_focal.tif"
+                },
+                {
+                    name:"Max_prec_df",
+                    alias:"Максимальное число осадков",
+                    periods:['1966_1995', '1971_2000','1976_2005', '1986_2015', '1981_2010'],
+                    months:['None'],
+                    style:"ucc:T_ave_year",
+                    postfix_in_file:"with_h_focal.tif"
                 }
             ]
         }
